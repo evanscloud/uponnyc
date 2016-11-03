73a8fa6c1de188247e8b5c93c0a357966efc7736
evanscloud <dijkstrascloud@gmail.com>
2016-11-03 14:03:02 -0400

    Add organization id to event

---
+++ db/migrate/20161103174232_add_organization_to_events.rb
@@ -0,0 +1,5 @@
+class AddOrganizationToEvents < ActiveRecord::Migration[5.0]
+  def change
+    add_column :events, :organization_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103024509) do
+ActiveRecord::Schema.define(version: 20161103174232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20161103024509) do
     t.text     "skills_needed"
     t.integer  "minimum_age"
     t.string   "url"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "organization_id"
   end
 
   create_table "organizations", force: :cascade do |t|
