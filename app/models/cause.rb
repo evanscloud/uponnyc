c29abb49c1c10f9cc7be9dab4ff8091d549d52f2
evanscloud <dijkstrascloud@gmail.com>
2016-11-03 12:02:41 -0400

    Fix other relationship errors

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,5 +1,4 @@
 class Cause < ApplicationRecord
-  belongs_to :organization
   has_many :causes_events
   has_many :events, :through => :causes_events
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+  belongs_to :organization
   has_many :causes_events
   has_many :causes, :through => :causes_events
 end
