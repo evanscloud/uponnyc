76b8707a011553359e49279bfac04170953d6526
evanscloud <dijkstrascloud@gmail.com>
2016-11-02 22:19:37 -0400

    Create cause resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102165731) do
+ActiveRecord::Schema.define(version: 20161103021705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "causes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
