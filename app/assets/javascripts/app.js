2760188293548a136abe3c91e2ea4c12f0e8a227
evanscloud <dijkstrascloud@gmail.com>
2016-11-09 12:45:41 -0500

    set and verify token

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,5 +4,9 @@
 
   angular
     .module('uponnyc', ['ui.router', 'templates'])
-
+    .config(function($httpProvider) {
+            // for CSRF errors
+            $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+        })
+        
 }());
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
   def index
   end
 
+  def set_csrf_cookie
+      cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
+
 end
