f0876ed5da4d231e27a6e1dddd8e16526fd8a4e8
evanscloud <dijkstrascloud@gmail.com>
2016-11-09 23:17:38 -0500

    Add causes to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Causes
+Cause.create(id: 23, name: "Advocacy & Human Rights")
+Cause.create(id: 30, name: "Animals")
+Cause.create(id: 34, name: "Arts & Culture")
+Cause.create(id: 38, name: "Board Development")
+Cause.create(id: 22, name: "Children & Youth")
+Cause.create(id: 25, name: "Community")
+Cause.create(id: 37, name: "Computers & Technology")
+Cause.create(id: 14, name: "Crisis Support")
+Cause.create(id: 17, name: "Disabled")
+Cause.create(id: 42, name: "Disaster Relief")
+Cause.create(id: 15, name: "Education & Literacy")
+Cause.create(id: 28, name: "Emergency & Safety")
+Cause.create(id: 27, name: "Employment")
+Cause.create(id: 13, name: "Environment")
+Cause.create(id: 31, name: "Gay Lesbian Bi & Trans")
+Cause.create(id: 11, name: "Health & Medicine")
+Cause.create(id: 7, name: "Homeless & Housing")
+Cause.create(id: 39, name: "Hunger")
+Cause.create(id: 41, name: "Immigrants & Refugees")
+Cause.create(id: 29, name: "International")
+Cause.create(id: 5, name: "Justice & Legal")
+Cause.create(id: 40, name: "Media & Broadcasting")
+Cause.create(id: 6, name: "Politics")
+Cause.create(id: 33, name: "Race & Ethnicity")
+Cause.create(id: 36, name: "Religion")
+Cause.create(id: 12, name: "Seniors")
+Cause.create(id: 19, name: "Sports & Recreation")
+Cause.create(id: 43, name: "Veterans & Military Families")
+Cause.create(id: 3, name: "Women")
