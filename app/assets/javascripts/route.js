ec9a7ebe78369ece40c82817265e8edd19177e48
evanscloud <dijkstrascloud@gmail.com>
2016-11-10 15:54:36 -0500

    Add causes route and button on home page

--- app/assets/javascripts/route.js
+++ app/assets/javascripts/route.js
@@ -8,8 +8,15 @@
       $stateProvider
         .state('home', {
           url: '/',
-          templateUrl: 'home/home.html'
+          templateUrl: 'home/home.html',
+          controller: 'HomeController as vm'
         })
+      $stateProvider
+        .state('causes', {
+          url: '/causes',
+          templateUrl: 'causes/causes.html'
+        })
+
 
       $urlRouterProvider.otherwise('/')
     })
