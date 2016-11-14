07f3c3f306fc26ae73eaf835e1d75adb451d3d1f
evanscloud <dijkstrascloud@gmail.com>
2016-11-13 21:43:23 -0500

    Remove event_type attribute causing AMS to fail

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :event_type, :start_time, :end_time, :start_date, :end_date, :description, :address, :skills_needed, :minimum_age, :url
+  attributes :id, :title, :start_time, :end_time, :start_date, :end_date, :description, :address, :skills_needed, :minimum_age, :url
 
   has_one :cause
 end
