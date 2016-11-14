39d8f279045484b266fbf366985ff9b1a957b79e
evanscloud <dijkstrascloud@gmail.com>
2016-11-13 23:31:36 -0500

    Add method to get individual cause data

--- app/assets/javascripts/causes/causes.service.js
+++ app/assets/javascripts/causes/causes.service.js
@@ -10,6 +10,12 @@
         .catch(handleError)
     };
 
+    this.getCause = function(cause) {
+      return $http.get('api/v1/causes/' + cause.id + '.json')
+        .then(handleSuccess)
+        .catch(handleError)
+    };
+
     function handleSuccess(response) {
       console.log(response);
       return response.data;
