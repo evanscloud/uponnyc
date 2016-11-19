7333acb67988b1813052795eccdce80104980357
evanscloud <dijkstrascloud@gmail.com>
2016-11-19 12:43:50 -0500

    Modify routes by adding shallow: true

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :causes, only: [:index, :show] do
-        resources :events, only: [:index, :show, :new, :create] do
+        resources :events, only: [:index, :show, :new, :create], shallow: true do
           resource :comments
         end
       end
