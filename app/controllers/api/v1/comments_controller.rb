d183d786a5051b33f9e2715dd68882706d137a49
evanscloud <dijkstrascloud@gmail.com>
2016-11-19 11:48:54 -0500

    Establish comments controller routes

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,33 @@
 module Api
   module V1
     class CommentsController < ApplicationController
+
+      def index
+        comments = Comment.all
+        render json: comments
+      end
+
+      def show
+        comment = Comment.find_by_id(params[:id])
+        render json: comment, include: ['event']
+      end
+
+      def create
+        event = Event.find_by_id(params[:event_id])
+        comment = event.comments.create(comment_params)
+        if comment.save
+          render json: comment
+        else
+          render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def comment_params
+        params.require(:comment).permit(:name, :content, :event_id)
+      end
+
     end
   end
 end
