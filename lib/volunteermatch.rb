38d1e497b0776fcc0de0c06bc74e2195645c2d34
evanscloud <dijkstrascloud@gmail.com>
2016-12-06 13:41:05 -0500

    Wrap api class inside VolunteerMatch module

--- lib/volunteermatch.rb
+++ lib/volunteermatch.rb
@@ -8,34 +8,38 @@ require 'uri'
 require 'json'
 require 'ostruct'
 
-class VolunteerMatchApi
+module VolunteerMatch
+  
+  class VolunteerMatchApi
 
-  def initialize()
-    @account_name = ENV['VM_ACCOUNT_NAME']
-    @api_key      = ENV['VM_ACCOUNT_KEY']
-  end
+    def initialize()
+      @account_name = ENV['VM_ACCOUNT_NAME']
+      @api_key      = ENV['VM_ACCOUNT_KEY']
+    end
 
-  def hello_world(name)
-    call :helloWorld, {:name => name}.to_json
-  end
+    def hello_world(name)
+      call :helloWorld, {:name => name}.to_json
+    end
 
-  protected
+    protected
 
-  def call(action, json_query)
-    nonce           = Digest::SHA2.hexdigest(rand.to_s)[0, 20]
-    creation_time   = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%S%z")
-    password_digest = Base64.encode64(Digest::SHA2.digest(nonce + creation_time + @api_key)).chomp
-    url             = URI.parse("http://www.volunteermatch.org/api/call?action=#{action.to_s}&query=" + URI.encode(json_query))
+    def call(action, json_query)
+      nonce           = Digest::SHA2.hexdigest(rand.to_s)[0, 20]
+      creation_time   = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%S%z")
+      password_digest = Base64.encode64(Digest::SHA2.digest(nonce + creation_time + @api_key)).chomp
+      url             = URI.parse("http://www.volunteermatch.org/api/call?action=#{action.to_s}&query=" + URI.encode(json_query))
 
-    req             = Net::HTTP::Get.new(url.request_uri)
-    req.add_field('Content-Type', 'application/json')
-    req.add_field('Authorization', 'WSSE profile="UsernameToken"')
-    req.add_field('X-WSSE', 'UsernameToken Username="' + @account_name + '", PasswordDigest="' + password_digest + '", ' +
-        'Nonce="' + nonce + '", Created="' + creation_time + '"')
+      req             = Net::HTTP::Get.new(url.request_uri)
+      req.add_field('Content-Type', 'application/json')
+      req.add_field('Authorization', 'WSSE profile="UsernameToken"')
+      req.add_field('X-WSSE', 'UsernameToken Username="' + @account_name + '", PasswordDigest="' + password_digest + '", ' +
+          'Nonce="' + nonce + '", Created="' + creation_time + '"')
 
-    res = Net::HTTP.new(url.host, url.port).start { |http| http.request(req) }
-    raise "HTTP error code #{res.code}" unless res.code == "200"
-    OpenStruct.new(JSON.parse res.body)
+      res = Net::HTTP.new(url.host, url.port).start { |http| http.request(req) }
+      raise "HTTP error code #{res.code}" unless res.code == "200"
+      OpenStruct.new(JSON.parse res.body)
+    end
+
   end
 
 end
