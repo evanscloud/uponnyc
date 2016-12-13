03214f7d806f9be22201142159d016599e885e1b
evanscloud <dijkstrascloud@gmail.com>
2016-12-13 14:43:17 -0500

    Create successful call to VolunteerMatch's api for results

--- app/controllers/api/v1/causes_controller.rb
+++ app/controllers/api/v1/causes_controller.rb
@@ -10,6 +10,8 @@ module Api
 
       def show
         cause = Cause.find_by_id(params[:id])
+        vm_cause = Volunteermatch.call(:searchOpportunities, {:location => "new york", :fieldsToDisplay => ["title", "parentOrg", "availability", "plaintextDescription", "location", "skillsNeeded", "minimumAge", "vmUrl"], :numberOfResults => 20, :sortOrder => "asc", :categoryIds => [params[:id].to_i]}.to_json)
+
         render json: cause, include: ['events']
       end
 
